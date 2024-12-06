Vekeng/Albion-CTA-Bot

commit 43f6f32d25846168294a80639154e33d6514cb42

FEATURE: Changed to use ES modules

bot.js
@@ -1,21 +1,20 @@
 // Discord.js imports
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, GatewayIntentBits, Events, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, PermissionFlagsBits, ApplicationRoleConnectionMetadata, isValidationEnabled } = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+import { REST } from '@discordjs/rest';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, GatewayIntentBits, Events, EmbedBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, PermissionFlagsBits, ApplicationRoleConnectionMetadata, isValidationEnabled } from 'discord.js';
+import { Routes } from 'discord-api-types/v9';
 
 // Core modules
-const path = require('path');
-const fs = require('fs');
-const { Console } = require('console');
+import path from 'path';
+import fs from 'fs';
 
 // Third-party modules
-const axios = require('axios');
-const Tesseract = require('tesseract.js');
-const dotenv = require('dotenv');
+import axios from 'axios';
+import Tesseract from 'tesseract.js';
+import dotenv from 'dotenv';
 
 // Internal modules
-const { botQueries, checkEvent, connectDb, disconnectDb, pgClient } = require('./postgres');
-const { Logger } = require('./utility');
+import { botQueries, checkEvent, connectDb, disconnectDb, pgClient } from './postgres.js';
+import { Logger } from './utility.js';
 
 // Initialize system logger
 global.systemlog = new Logger();

commit df7bac019be383ab2024778da3cc6f4de4aa7d83

FIX: Pressumably commit  43f6f32d25846168294a80639154e33d6514cb42 caused some changes in app behaviour. It caused a lot of undefined variable exceptions. This is fox for it

bot.js
@@ -13,9 +13,19 @@ import Tesseract from 'tesseract.js';
 import dotenv from 'dotenv';
 
 // Internal modules
-import { botQueries, checkEvent, connectDb, disconnectDb, pgClient } from './postgres.js';
+import { botQueries, connectDb, disconnectDb, pgClient } from './postgres.js';
 import { Logger } from './logger.js';
 import { commands } from './commands.js';
+import { 
+    eventExists, 
+    combineDateAndTime, 
+    isValidNumber, 
+    getMessage, 
+    extractKeywordAndTime, 
+    isValidTime, 
+    isDateValid, 
+    buildEventMessage 
+} from './functions.js';
 
 // Initialize system logger
 global.systemlog = new Logger();
@@ -23,174 +33,7 @@ global.systemlog = new Logger();
 // Load environment variables
 dotenv.config();
 
-async function eventExists(eventMessage, eventId, guildId, interaction) {
-    if (await checkEvent(eventId, guildId) === 0 ) {
-        if (eventMessage) {
-            // If event is not in db, but exists in channel - delete it
-            eventMessage.delete();
-        }
-        return false;
-    }
-    return true;
-}
 
-function combineDateAndTime(dateStr, timeStr) {
-    // Parse the date string (DD.MM.YYYY)
-    const [day, month, year] = dateStr.split('.').map(Number);
-  
-    // Extract hours and minutes from the UTC time string (HH:MM)
-    const [hours, minutes] = timeStr.split(':').map(Number);
-  
-    // Create a Date object with the UTC time and parsed date
-    // We need to use the format YYYY-MM-DDTHH:MM:00Z for UTC date-time
-    const dateTimeString = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}T${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:00Z`;
-    
-    // Return a Date object
-    return new Date(dateTimeString);
-  }
-
-function isValidNumber(value) {
-    const regex = /^\d+$/; // This regex checks for one or more digits
-    if (regex.test(value) && value <= 9223372036854775807) {
-        return true;
-    } else {
-        logger.error(`Not a valid snowflake: ${value}`);
-        return false;
-    }
-}
-
-async function getMessage(interaction, messageId) {
-    try {
-        // Try to fetch the message by its ID
-        const message = await interaction.channel.messages.fetch(messageId);
-        return message;  // Return message
-    } catch (error) {
-        if (error.code === 10008) {  // Unknown Message error code
-            logger.error(`Message ${messageId} does not exist`);
-            return null;  // Return null
-        } else {
-            logger.error(`An error occurred when retreiving the message: ${messageId}`, error.stack);
-            throw error;  // Some other error occurred
-        }
-    }
-}
-
-function extractKeywordAndTime(message, keyword) {
-    // Regex for "X h Y m" format (hours and minutes)
-    const timeRegexHoursMinutes = /(\d+)\s*h\s*(\d{2})/;
-    // Regex for "X m Y s" format (minutes and seconds)
-    const timeRegexMinutesSeconds = /(\d+)\s*m\s*(\d{2})/;
-    // Regex for "X h" format (only hours)
-    const timeRegexOnlyHours = /(\d+)\s*h/;
-    // Regex for "X m" format (only minutes)
-    const timeRegexOnlyMinutes = /(\d+)\s*m/;
-    
-    const timeMatchHoursMinutes = message.match(timeRegexHoursMinutes);
-    const timeMatchMinutesSeconds = message.match(timeRegexMinutesSeconds);
-    const timeMatchOnlyHours = message.match(timeRegexOnlyHours);
-    const timeMatchOnlyMinutes = message.match(timeRegexOnlyMinutes);
-
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let totalSeconds = 0;
-    const unixTimeNow = Math.floor(Date.now() / 1000);
-    let unixTimeContent = unixTimeNow; // Default to current time if no match
-
-    if (timeMatchHoursMinutes) {
-        // If we matched "X h Y m" format
-        hours = parseInt(timeMatchHoursMinutes[1], 10);
-        minutes = parseInt(timeMatchHoursMinutes[2], 10);
-        totalSeconds = (hours * 3600) + (minutes * 60); // Convert to seconds
-        unixTimeContent = unixTimeNow + totalSeconds;
-    } else if (timeMatchMinutesSeconds) {
-        // If we matched "X m Y s" format
-        minutes = parseInt(timeMatchMinutesSeconds[1], 10);
-        seconds = parseInt(timeMatchMinutesSeconds[2], 10);
-        totalSeconds = (minutes * 60) + seconds; // Convert to seconds
-        unixTimeContent = unixTimeNow + totalSeconds;
-    } else if (timeMatchOnlyHours) {
-        // If we matched only "X h" format
-        hours = parseInt(timeMatchOnlyHours[1], 10);
-        totalSeconds = hours * 3600; // Convert to seconds
-        unixTimeContent = unixTimeNow + totalSeconds;
-    } else if (timeMatchOnlyMinutes) {
-        // If we matched only "X m" format
-        minutes = parseInt(timeMatchOnlyMinutes[1], 10);
-        totalSeconds = minutes * 60; // Convert to seconds
-        unixTimeContent = unixTimeNow + totalSeconds;
-    }
-
-    // Return the calculated Unix timestamp
-    return unixTimeContent;
-}
-
-function isValidTime(time) {
-    // Regular expression to match HH:MM format
-    const timePattern = /^([0-1][0-9]|2[0-3]):([0-5][0-9])$/;
-    return timePattern.test(time);
-}
-
-function isDateValid(dateString) {
-    // Regular expression to match DD.MM.YYYY format
-    const regex = /^\d{2}\.\d{2}\.\d{4}$/;
-
-    // Check if it matches the regex
-    if (!regex.test(dateString)) {
-        return false;
-    }
-
-    // Split the string into day, month, and year
-    const [day, month, year] = dateString.split('.').map(Number);
-    // Check if the month is valid (1-12)
-    if (month < 1 || month > 12) {
-        return false;
-    }
-
-    // Check if the day is valid for the given month
-    const daysInMonth = new Date(year, month, 0).getDate(); // 0th day of next month gives last day of this month
-    if (day < 1 || day > daysInMonth) {
-        return false;
-    }
-
-    return true;
-}
-
-function buildEventMessage(eventParticipants, eventDetails) {
-    const embed = new EmbedBuilder()
-        .setTitle(eventDetails.event_name)
-        .setDescription(`Date: **${eventDetails.date}**\nTime (UTC): **${eventDetails.time_utc}**`)
-        .setColor('#0099ff');
-    // Group roles by party
-    const groupedRoles = eventParticipants.reduce((acc, { role_id, role_name, party, user_id }) => {
-        if (!acc[party]) acc[party] = []; // Create a new array for the party if it doesn't exist
-        acc[party].push({ role_id, role_name, user_id }); // Add role to the party group
-        return acc;
-    }, {});
-
-    // Iterate through each party and format roles
-    for (const party in groupedRoles) {
-        let partyRoles = ''; // Initialize the role list for this party
-
-        // Process each role in the party
-        groupedRoles[party].forEach(({ role_id, role_name, user_id }) => {
-            // Check if there's a participant for the role
-            const status = user_id ? `<@${user_id}>` : 'Available'; // Format mention or 'Available'
-            
-            // Add formatted role to the party's list
-            if (status === 'Available') {
-                partyRoles += `\`🟩\` ${role_id}. ${role_name}\n`; // Available roles with green square
-            } else {
-                partyRoles += `\`✔️\` ${role_id}. ${role_name} - ${status}\n`; // Roles with a participant
-            }
-        });
-
-        // Add the formatted roles list to the embed
-        embed.addFields({ name: `⚔️ ${party}`, value: partyRoles, inline: true });
-    }
-    embed.setFooter({text: `Event ID: ${eventDetails.event_id}`});
-    return embed;
-}
 
 const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
 
@@ -291,7 +134,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                 if (interaction.customId.startsWith('ctaping')) {
                     const [action, messageId] = interaction.customId.split('|');
                     const eventMessage = await getMessage(interaction, messageId); 
-                    response = "";
+                    let response;
                     if (!await eventExists(eventMessage, messageId, guildId, interaction)) {
                         return await interaction.reply({ content: 'Event doesn\'t exist in this channel', ephemeral: true});
                     }
@@ -326,7 +169,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                     const eventParticipants = await pgClient.query(botQueries.GET_EVENT_PARTICIPANTS, [messageId, guildId]);
                     const eventDataResult = await pgClient.query(botQueries.GET_EVENT, [messageId, guildId]); 
                     const eventDetails = eventDataResult.rows[0];
-                    embed = buildEventMessage(eventParticipants.rows, eventDetails);
+                    const embed = buildEventMessage(eventParticipants.rows, eventDetails);
                     await eventMessage.edit({ embeds: [embed] });
                     await interaction.reply({ content: `You have successfully left your role.`, ephemeral: true });
                 }
@@ -339,7 +182,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                         return await interaction.reply({ content: 'Event doesn\'t exist in this channel', ephemeral: true});
                     }
                     try {
-                        availablePartiesResult = await pgClient.query(botQueries.GET_AVAILABLE_PARTIES, [messageId, guildId]);
+                        const availablePartiesResult = await pgClient.query(botQueries.GET_AVAILABLE_PARTIES, [messageId, guildId]);
 
                         for (const party of availablePartiesResult.rows) {
                             options.push({
@@ -416,7 +259,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                     if (!await eventExists(eventMessage, messageId, guildId, interaction)) {
                         return await interaction.reply({ content: 'Event doesn\'t exist in this channel', ephemeral: true});
                     }
-                    availableRolesInPartyResult = await pgClient.query(botQueries.GET_AVAILABLE_ROLES_IN_PARTY, [messageId, guildId, party]);
+                    const availableRolesInPartyResult = await pgClient.query(botQueries.GET_AVAILABLE_ROLES_IN_PARTY, [messageId, guildId, party]);
                     const options = [];
                     for (const { role_id: roleId, role_name: roleName } of availableRolesInPartyResult.rows) {
                         options.push({
@@ -481,6 +324,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                         return await interaction.reply({ content: 'Event doesn\'t exist in this channel', ephemeral: true});   
                     }   
                     const { rows : eventData } = await pgClient.query(botQueries.GET_EVENT, [messageId, guildId]);
+                    let embed;
                     if ( eventData.length === 1 ) {
                         
                         const removeParticipantQuery = 'DELETE FROM participants WHERE role_id=ANY($1) AND event_id=$2 AND discord_id=$3';
@@ -501,6 +345,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                     try {
                         await interaction.deferReply({ephemeral: true});
                         const response = await axios.get(attachment.url, { responseType: 'arraybuffer' });
+                        const __dirname = path.dirname(new URL(import.meta.url).pathname);
                         const imagePath = path.join(__dirname, 'temp_image.png');
 
                         // Save the image temporarily
@@ -546,8 +391,8 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                 if (subCommand === 'myctas') {
                     const { rows : myCtaRows } = await pgClient.query(botQueries.GET_MYCTAS, [userId, guildId]);
                     if ( myCtaRows.length > 0 ) {
-                        message = 'Upcoming events: \n';
-                        for ( row of myCtaRows ) {
+                        let message = 'Upcoming events you are signed up for: \n';
+                        for ( const row of myCtaRows ) {
                             const today = new Date();
                             const dateTime = combineDateAndTime(row.date, row.time_utc);
                             if (dateTime.getTime() >= today.getTime()) {
@@ -598,7 +443,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                         await pgClient.query(removeParticipantQuery, [removedUsers, messageId, guildId]);
                     }
                     const { rows : participantsAfter } = await pgClient.query(botQueries.GET_EVENT_PARTICIPANTS, [messageId, guildId]);
-                    embed = buildEventMessage(participantsAfter, eventDetails);
+                    const embed = buildEventMessage(participantsAfter, eventDetails);
                     await eventMessage.edit({ embeds: [embed] });
                     await interaction.reply({ content: `Users ${removedUsers.map(user => `<@${user}>`).join(', ')} have been cleared.`, ephemeral: true });
                 }
@@ -636,7 +481,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                     const date = options.getString('date');
                     const timeUTC = options.getString('time');
                     const compName = options.getString('comp');
-                    //const participants = {};
+                    let eventParticipants;
                                 
                     const getComps = `SELECT comp_name FROM compositions WHERE comp_name=$1 AND discord_id=$2;`;
                     const { rows : compsRows } = await pgClient.query(getComps, [compName, guildId])
@@ -649,7 +494,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                     if (!isValidTime(timeUTC)) {
                         return await interaction.reply({ content: `${timeUTC} is not valid time. Time must be in HH:MM format`, ephemeral: true });
                     }
-                    message = await interaction.deferReply({ fetchReply: true });
+                    const message = await interaction.deferReply({ fetchReply: true });
                     const eventId = message.id
                     const eventDetails = {
                         event_id: eventId,
@@ -691,7 +536,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                         .setStyle(ButtonStyle.Danger);
                 
                     const actionRow = new ActionRowBuilder().addComponents(joinButton, leaveButton, pingButton);
-                    embed = buildEventMessage(eventParticipants.rows, eventDetails)
+                    const embed = buildEventMessage(eventParticipants.rows, eventDetails)
                     embed.setFooter({ text: `Event ID: ${eventId}` });
                     await interaction.editReply({
                         embeds: [embed],
@@ -750,6 +595,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
                         // Composition exists, send a reply
                         return await interaction.reply({ content: `Composition "${compName}" already exists.`, ephemeral: true });
                     } 
+                    let response; 
                     try {
                         // Start a transaction to insert the composition and its roles
                         await pgClient.query('BEGIN');

functions.js
@@ -1,0 +1,179 @@
+import { EmbedBuilder } from 'discord.js';
+import { pgClient } from './postgres.js';
+
+
+const checkEvent = async (eventId, guildId) => {
+    const selectEventQuery = `SELECT * FROM events WHERE event_id=$1 and discord_id=$2;`;
+    const selectResult = await pgClient.query(selectEventQuery, [eventId, guildId]);
+    const eventCount = selectResult.rowCount;
+    return eventCount;
+};
+
+export async function eventExists(eventMessage, eventId, guildId) {
+    if (await checkEvent(eventId, guildId) === 0 ) {
+        if (eventMessage) {
+            // If event is not in db, but exists in channel - delete it
+            eventMessage.delete();
+        }
+        return false;
+    }
+    return true;
+}
+
+export function combineDateAndTime(dateStr, timeStr) {
+    // Parse the date string (DD.MM.YYYY)
+    const [day, month, year] = dateStr.split('.').map(Number);
+  
+    // Extract hours and minutes from the UTC time string (HH:MM)
+    const [hours, minutes] = timeStr.split(':').map(Number);
+  
+    // Create a Date object with the UTC time and parsed date
+    // We need to use the format YYYY-MM-DDTHH:MM:00Z for UTC date-time
+    const dateTimeString = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}T${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:00Z`;
+    
+    // Return a Date object
+    return new Date(dateTimeString);
+  }
+
+export function isValidNumber(value) {
+    const regex = /^\d+$/; // This regex checks for one or more digits
+    if (regex.test(value) && value <= 9223372036854775807) {
+        return true;
+    } else {
+        logger.error(`Not a valid snowflake: ${value}`);
+        return false;
+    }
+}
+
+export async function getMessage(interaction, messageId) {
+    try {
+        // Try to fetch the message by its ID
+        const message = await interaction.channel.messages.fetch(messageId);
+        return message;  // Return message
+    } catch (error) {
+        if (error.code === 10008) {  // Unknown Message error code
+            logger.error(`Message ${messageId} does not exist`);
+            return null;  // Return null
+        } else {
+            logger.error(`An error occurred when retreiving the message: ${messageId}`, error.stack);
+            throw error;  // Some other error occurred
+        }
+    }
+}
+
+export function extractKeywordAndTime(message, keyword) {
+    // Regex for "X h Y m" format (hours and minutes)
+    const timeRegexHoursMinutes = /(\d+)\s*h\s*(\d{2})/;
+    // Regex for "X m Y s" format (minutes and seconds)
+    const timeRegexMinutesSeconds = /(\d+)\s*m\s*(\d{2})/;
+    // Regex for "X h" format (only hours)
+    const timeRegexOnlyHours = /(\d+)\s*h/;
+    // Regex for "X m" format (only minutes)
+    const timeRegexOnlyMinutes = /(\d+)\s*m/;
+    
+    const timeMatchHoursMinutes = message.match(timeRegexHoursMinutes);
+    const timeMatchMinutesSeconds = message.match(timeRegexMinutesSeconds);
+    const timeMatchOnlyHours = message.match(timeRegexOnlyHours);
+    const timeMatchOnlyMinutes = message.match(timeRegexOnlyMinutes);
+
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let totalSeconds = 0;
+    const unixTimeNow = Math.floor(Date.now() / 1000);
+    let unixTimeContent = unixTimeNow; // Default to current time if no match
+
+    if (timeMatchHoursMinutes) {
+        // If we matched "X h Y m" format
+        hours = parseInt(timeMatchHoursMinutes[1], 10);
+        minutes = parseInt(timeMatchHoursMinutes[2], 10);
+        totalSeconds = (hours * 3600) + (minutes * 60); // Convert to seconds
+        unixTimeContent = unixTimeNow + totalSeconds;
+    } else if (timeMatchMinutesSeconds) {
+        // If we matched "X m Y s" format
+        minutes = parseInt(timeMatchMinutesSeconds[1], 10);
+        seconds = parseInt(timeMatchMinutesSeconds[2], 10);
+        totalSeconds = (minutes * 60) + seconds; // Convert to seconds
+        unixTimeContent = unixTimeNow + totalSeconds;
+    } else if (timeMatchOnlyHours) {
+        // If we matched only "X h" format
+        hours = parseInt(timeMatchOnlyHours[1], 10);
+        totalSeconds = hours * 3600; // Convert to seconds
+        unixTimeContent = unixTimeNow + totalSeconds;
+    } else if (timeMatchOnlyMinutes) {
+        // If we matched only "X m" format
+        minutes = parseInt(timeMatchOnlyMinutes[1], 10);
+        totalSeconds = minutes * 60; // Convert to seconds
+        unixTimeContent = unixTimeNow + totalSeconds;
+    }
+
+    // Return the calculated Unix timestamp
+    return unixTimeContent;
+}
+
+export function isValidTime(time) {
+    // Regular expression to match HH:MM format
+    const timePattern = /^([0-1][0-9]|2[0-3]):([0-5][0-9])$/;
+    return timePattern.test(time);
+}
+
+export function isDateValid(dateString) {
+    // Regular expression to match DD.MM.YYYY format
+    const regex = /^\d{2}\.\d{2}\.\d{4}$/;
+
+    // Check if it matches the regex
+    if (!regex.test(dateString)) {
+        return false;
+    }
+
+    // Split the string into day, month, and year
+    const [day, month, year] = dateString.split('.').map(Number);
+    // Check if the month is valid (1-12)
+    if (month < 1 || month > 12) {
+        return false;
+    }
+
+    // Check if the day is valid for the given month
+    const daysInMonth = new Date(year, month, 0).getDate(); // 0th day of next month gives last day of this month
+    if (day < 1 || day > daysInMonth) {
+        return false;
+    }
+
+    return true;
+}
+
+export function buildEventMessage(eventParticipants, eventDetails) {
+    const embed = new EmbedBuilder()
+        .setTitle(eventDetails.event_name)
+        .setDescription(`Date: **${eventDetails.date}**\nTime (UTC): **${eventDetails.time_utc}**`)
+        .setColor('#0099ff');
+    // Group roles by party
+    const groupedRoles = eventParticipants.reduce((acc, { role_id, role_name, party, user_id }) => {
+        if (!acc[party]) acc[party] = []; // Create a new array for the party if it doesn't exist
+        acc[party].push({ role_id, role_name, user_id }); // Add role to the party group
+        return acc;
+    }, {});
+
+    // Iterate through each party and format roles
+    for (const party in groupedRoles) {
+        let partyRoles = ''; // Initialize the role list for this party
+
+        // Process each role in the party
+        groupedRoles[party].forEach(({ role_id, role_name, user_id }) => {
+            // Check if there's a participant for the role
+            const status = user_id ? `<@${user_id}>` : 'Available'; // Format mention or 'Available'
+            
+            // Add formatted role to the party's list
+            if (status === 'Available') {
+                partyRoles += `\`🟩\` ${role_id}. ${role_name}\n`; // Available roles with green square
+            } else {
+                partyRoles += `\`✔️\` ${role_id}. ${role_name} - ${status}\n`; // Roles with a participant
+            }
+        });
+
+        // Add the formatted roles list to the embed
+        embed.addFields({ name: `⚔️ ${party}`, value: partyRoles, inline: true });
+    }
+    embed.setFooter({text: `Event ID: ${eventDetails.event_id}`});
+    return embed;
+}

postgres.js
@@ -1,6 +1,6 @@
 import pg from 'pg';  // Import pg module
 const { Client } = pg;  // Destructure Client from pg module
-import { Logger } from './utility.js';
+import { Logger } from './logger.js';
 
 import dotenv from 'dotenv';
 const logger = new Logger();
@@ -31,13 +31,6 @@ const disconnectDb = async () => {
   } catch (error) {
     logger.error('Error disconnecting from the database:', error.stack);
   }
-};
-
-const checkEvent = async (eventId, guildId) => {
-  const selectEventQuery = `SELECT * FROM events WHERE event_id=$1 and discord_id=$2;`;
-  const selectResult = await pgClient.query(selectEventQuery, [eventId, guildId]);
-  const eventCount = selectResult.rowCount;
-  return eventCount;
 };
 
 const botQueries = {
@@ -140,4 +133,4 @@ const botQueries = {
     `
 }
 
-export { botQueries, checkEvent, connectDb, disconnectDb, pgClient };
+export { botQueries, connectDb, disconnectDb, pgClient };